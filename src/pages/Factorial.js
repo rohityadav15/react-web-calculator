2e707d60f0780575932d936c86cadc1ed976234a
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-05 22:48:14 +0530

    Factorial

--- src/pages/Factorial.js
+++ src/pages/Factorial.js
@@ -8,10 +8,25 @@ const Factorial = () => {
     setFactorialValue(e.target.value);
   };
   const handleSubmit = () => {};
-console.log("======>", factorialValue)
+  console.log("======>", factorialValue);
   return (
-    <>
-      <Box style={{display:"flex",flexDirection:"row",justifyContent:"center", alignItems:"center", marginTop:50}}>
+    <Box
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 50,
+        }}
+      >
         <Typography> Enter the value</Typography>
         <TextField
           label="Principale"
@@ -19,12 +34,22 @@ console.log("======>", factorialValue)
           size="small"
           type="Number"
           onChange={handleFactorialValue}
-          style={{ marginBottom: "20px", marginLeft: 20, marginTop:20}}
+          style={{ marginBottom: "20px", marginLeft: 20, marginTop: 20 }}
         />
-       
       </Box>
-      <Button onClick={handleSubmit} style={{display:"flex", justifyContent:"center",alignItems:"centergit"}}>Factorial</Button>
-    </>
+      <Box>
+        <Button
+          onClick={handleSubmit}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "centergit",
+          }}
+        >
+          Factorial
+        </Button>
+      </Box>
+    </Box>
   );
 };
 export default Factorial;
