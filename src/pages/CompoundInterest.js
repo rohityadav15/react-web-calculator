c3f86fec2055dcaf23c6041b6e3fe4499d459903
Rohit Kumar Yadav <rohit.yadav102002@gmail.com>
2024-12-13 22:59:02 +0530

    Compound layout

--- src/pages/CompoundInterest.js
+++ src/pages/CompoundInterest.js
@@ -1,9 +1,39 @@
-import { Box } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 
 const CompoundInterest = () => {
   return (
-    <Box>
-      <Box>Calculate</Box>
+    <Box style={{display:"flex", flexDirection:"row"}}>
+      <Box>
+        <Typography>Amount (A)</Typography>
+        <Typography>Principle(P)</Typography>
+
+        <Typography>Interest(R)</Typography>
+        <Typography>Number of interest in year (N)</Typography>
+        <Typography>Time in Year(T)</Typography>
+      </Box>
+      <Box style={{display:"flex", flexDirection:"column"}}>
+        <TextField 
+        type="Numer"
+        size="small"
+        />
+         <TextField 
+        type="Numer"
+        size="small"
+        />
+         <TextField 
+        type="Numer"
+        size="small"
+        />
+         <TextField 
+        type="Numer"
+        size="small"
+        />
+         <TextField 
+        type="Numer"
+        size="small"
+        />
+
+      </Box>
     </Box>
   );
 };
